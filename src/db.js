deffa95b5ea061d363ce3cf6f5c1d6b021b59833
Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
2024-01-12 10:49:37 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ dotenv.config({path: '.env'});
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb://mongo:gCf65Fae3DEED2G-DcBccbEG3fEGddCB@monorail.proxy.rlwy.net:31944");
         console.log(">>> MongoDB Connected...");
     } catch (error) {
         console.log(error);
