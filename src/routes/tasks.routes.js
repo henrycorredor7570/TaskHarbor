bb985fa159b688b7e50b912724c546345aba45e2
Henry Corredor González <henry.corredor7570@gmail.com>
2024-01-18 17:35:30 -0500

    delete token

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,10 +7,10 @@ import { createTaskSchema } from "../schemas/task.schema.js";
 
 const router = Router();
 
-router.get("/tasks", authRequired, getTasks );
-router.get("/tasks/:id", authRequired, getTask);
-router.post("/tasks", authRequired, validateSchema(createTaskSchema), createTask);
-router.delete("/tasks/:id", authRequired, deleteTask);
-router.put("/tasks/:id", authRequired, updateTask);
+router.get("/tasks", getTasks );
+router.get("/tasks/:id", getTask);
+router.post("/tasks", validateSchema(createTaskSchema), createTask);
+router.delete("/tasks/:id", deleteTask);
+router.put("/tasks/:id", updateTask);
 
 export default router;
