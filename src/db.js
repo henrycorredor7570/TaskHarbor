086ad7bfe5d855ba0846ac996b026418e6fe031b
Henry Corredor González <henry.corredor7570@gmail.com>
2024-01-18 14:40:19 -0500

    mongoose db connect

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-dotenv.config({path: '.env'});
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/taskharbor");
-        console.log(">>> MongoDB Connected...");
-    } catch (error) {
-        console.log(error);
-    }
-}
-//process.env.MONGODB_URL
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log(">>> MongoDB Connected...");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
