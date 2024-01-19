838b4784e57902bae53b04afc45d88bda231644a
Henry Corredor González <henry.corredor7570@gmail.com>
2024-01-18 19:38:11 -0500

    validateToken

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -5,7 +5,7 @@ import { TOKEN_SECRET } from '../config.js';
 export const authRequired  = (req, res, next) => {
     const { token } = req.cookies;
 
-    if(!token) return res.status(401).json({message: "No token, authorization denied"});
+    if(!token) return res.status(401).json({message: "No token, authorization denied no puede pasar "});
 
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
         if(err) return res.status(403).json({message: "Invalid token"});
